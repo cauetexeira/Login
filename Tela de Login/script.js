4dde5df9103a171d82c458d2275e172f8e61857a
cauetexeira <160753286+cauetexeira@users.noreply.github.com>
2024-04-16 12:36:54 -0300

    Add files via upload

---
+++ Tela de Login/script.js
@@ -0,0 +1,6 @@
+function Dados() {
+    var nome = document.getElemenetById("Nome").Value;
+    var senha = document.getElemenetById("Senha").Value;
+    var idade = document.getElemenetById("Idade").Value;
+    var email = document.getElemenetById("Email").Value;
+}
